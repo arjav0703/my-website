276f9b2f65d35bfb963e2fa935fdd8a9a24d4a49
arjav0703 <arjavjain0703@gmail.com>
2025-03-03 17:34:18 +0530

    new bg!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center py-20">
-      <h1 className="text-7xl">ARJAV JAIN</h1>
-      <p>I am arjav</p>
+    <div className="py-20 flex flex-col items-center bg-amber-50 h-screen">
       
+      <div>
+        <h1 className="text-6xl">ARJAV JAIN</h1>
+        <p className="my-15 text-2xl">I love to code and try new things.</p>
+      </div>
     </div>
   );
 }
