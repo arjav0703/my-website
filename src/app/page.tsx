49a6819f55bee5922ac7b09e288ea33a954f5070
arjav0703 <arjavjain0703@gmail.com>
2025-03-03 19:51:09 +0530

    add my stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,15 @@ export default function Home() {
       
       <div>
         <h1 className="text-5xl">ARJAV JAIN</h1>
-        <p className="py-10 text-xl ">
-        i am a 14 year old from Vidisha. i really dont't have a fav language and i love to try almost everything from 3D modelling to functional programming, linux and tons of other cool stuff.
-        </p>
+
+        <div className="flex flex-row py-1">
+          <p className="underline mx-2">photograper</p> 
+          <p className="underline mx-2">ethical hacker</p>
+
+        </div>
+          <p className="py-5 text-xl ">
+          i am a 14 year old from Vidisha. i really dont't have a fav language but i love to try almost everything from 3D modelling to functional programming, linux and tons of other cool stuff.
+          </p>
         
         <p>i am a<a href='https://hackclub.com' className=" px-1 hover:bg-amber-500 hover:underline">[Hackclubber]</a>and the co-leader at<a href="https://vidisha.hackclub.com" className=" px-1 hover:bg-amber-500 hover:underline">[hackclub vidisha]</a>.</p>
         
