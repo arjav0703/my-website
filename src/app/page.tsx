4563a0073531b117c448ba03c69193e562d866af
arjav0703 <arjavjain0703@gmail.com>
2025-03-03 18:25:30 +0530

    this is fast!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="py-20 flex flex-col items-center bg-amber-50 h-screen">
+    <div className="py-20 flex flex-col items-center bg-amber-50 h-screen px-18">
       
       <div>
         <h1 className="text-6xl">ARJAV JAIN</h1>
-        <p className="my-15 text-2xl">I love to code and try new things.</p>
+        <p className="py-10 text-xl ">
+        I am a 14 year old from Vidisha. I really doesn't have a fav language and love to try almost everything from 3D modelling to functional programming and linux.
+        </p>
+        <p>another para <a href="https://vidisha.hackclub.com" className=" px-1 hover:bg-amber-500 hover:underline">Hackclub Vidisha</a> </p>
       </div>
     </div>
   );
