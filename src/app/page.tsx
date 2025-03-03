65f59a5e8229d6f948c44dd7bb70f30ba5202330
arjav0703 <arjavjain0703@gmail.com>
2025-03-03 18:27:31 +0530

    fix grammar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div>
         <h1 className="text-6xl">ARJAV JAIN</h1>
         <p className="py-10 text-xl ">
-        I am a 14 year old from Vidisha. I really doesn't have a fav language and love to try almost everything from 3D modelling to functional programming and linux.
+        I am a 14 year old from Vidisha. I really dont't have a fav language and love to try almost everything from 3D modelling to functional programming and linux.
         </p>
         <p>another para <a href="https://vidisha.hackclub.com" className=" px-1 hover:bg-amber-500 hover:underline">Hackclub Vidisha</a> </p>
       </div>
