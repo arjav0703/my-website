c9268915ee74e3169f05846cd9e47bbbf1a83816
arjav0703 <arjavjain0703@gmail.com>
2025-03-03 16:18:31 +0530

    this is confusing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Monoton} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const monoton = Monoton({
+  variable: "--font-monoton",
+  weight: "400"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Home",
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${monoton.variable} ${monoton.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-row items-center justify-center min-h-screen py-2">
+      <h1 className="font-monoton">Home</h1>
+      <h1 className="">Home</h1>
       
     </div>
   );
