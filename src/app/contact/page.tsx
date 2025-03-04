fb76f9da48ed561cce7a521893ed8345a14f56b0
arjav0703 <arjavjain0703@gmail.com>
2025-03-04 21:06:10 +0530

    add discord to contact details

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,6 +18,8 @@ export default function contact() {
             <div className="py-5">
                 <p className="text-l sm:text-xl">You can email me at</p>
                 <p className="text-l sm:text-xl"> <a href="mailto:arjavjain0703@gmail.com" className="hover:underline hover:bg-amber-100">arjavjain0703@gmail.com</a> or <a href="mailto:arjav@hackclub.app" className="hover:underline hover:bg-amber-100">arjav@hackclub.app</a></p>
+
+                <p className="text-l sm:text-xl py-5 lg:py:2">You can also find me on Discord <a href='https://discordapp.com/users/1265239979143987273' className='hover:underline bg-gruv-blue text-white px-1'>@madmax000108</a></p>
             </div>
   
         </div>
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import  MyFooter  from '../../components/footer';
-export default function contact() {
+export default function gallery() {
     return (
       <div className="bg-gruv-bg text-gruv-text selection:bg-gruv-selection w-full">
         <div className="flex justify-end py-5 px-18">
@@ -15,20 +15,20 @@ export default function contact() {
             <div>
                 <h1 className="text-xl sm:text-3xl">GALLERY</h1>
             </div>
-            <div className='sm:grid sm:grid-cols-4 sm:grid-rows-4 sm:gap-5 sm:w-screen sm:px-10 py-10  '>
-                <img src='/images/car.webp' alt='closeup shot of a toy car'></img>
-                <img src='/images/ole.webp' alt='close up shot of a Jewellery'></img>
-                <img src='/images/apple.webp' alt='close up shot of an apple'></img>
-                <img src='/images/oldman.webp' alt='picture of a street vendor selling clothes'></img>
-                <img src='/images/trash.webp' alt='picture of an abandoned street'></img>
-                <img src='/images/boat2.webp' alt='picure of a boat during sunset'></img>
-                <img src='/images/light.webp' alt='shot of a light in darkness'></img>
-                <img src='/images/window.webp' alt='rays of sun pouring in through a window'></img>
-                <img src='/images/tree.webp' alt='a blurred image a tree during rain'></img>
-                <img src='/images/lens.webp' alt='macro shot of a camera lens'></img>
-                <img src='/images/lego.webp' alt='cinematic image of a lego plane model'></img>
-                <img src='/images/leaf.webp' alt='close up shot of water droplets on a green leaf'></img>
-            </div>
+            <section className='sm:grid sm:grid-cols-4 sm:gap-5 sm:w-screen sm:px-10 py-10  '>
+                <img src='/images/car.webp' alt='closeup shot of a toy car'className='flex'></img>
+                <img src='/images/ole.webp' alt='close up shot of a Jewellery'className='flex'></img>
+                <img src='/images/apple.webp' alt='close up shot of an apple'className='flex'></img>
+                <img src='/images/oldman.webp' alt='picture of a street vendor selling clothes'className='flex'></img>
+                <img src='/images/trash.webp' alt='picture of an abandoned street'className='flex'></img>
+                <img src='/images/boat2.webp' alt='picure of a boat during sunset'className='flex'></img>
+                <img src='/images/light.webp' alt='shot of a light in darkness'className='flex'></img>
+                <img src='/images/window.webp' alt='rays of sun pouring in through a window'className='flex'></img>
+                <img src='/images/tree.webp' alt='a blurred image a tree during rain'className='flex'></img>
+                <img src='/images/lens.webp' alt='macro shot of a camera lens'className='flex'></img>
+                <img src='/images/lego.webp' alt='cinematic image of a lego plane model'className='flex'></img>
+                <img src='/images/leaf.webp' alt='close up shot of water droplets on a green leaf'className='flex'></img>
+            </section>
             
   
         </div>
