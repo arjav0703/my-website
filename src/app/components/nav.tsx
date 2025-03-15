0652fe3b27df3f093c75e28fb73420d8c2a4ba7d
Arjav Jain <arjavjain0703@gmail.com>
2025-03-15 16:02:55 +0000

    add knowledgebase to navbar

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,6 +4,7 @@ export default function MyNav() {
             <nav className="flex flex-row gap-5">
             <a href='./' className="hover:underline">about</a>
             <a href='./gallery' className="hover:underline">gallery</a>
+            <a href='https://knowledgebase.arjav.dino.icu' className="hover:underline">knowledgebase</a>
             <a href='./contact' className="hover:underline">contact</a>
             </nav>
         </div>
