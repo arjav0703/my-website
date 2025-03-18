47dc19dea4b4ad38953bb3575230a6e956019095
Arjav Jain <arjavjain0703@gmail.com>
2025-03-18 15:42:14 +0000

    add alert

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CopyHandler from '../components/CopyHandler';
+import { useEffect } from 'react';
 
 const blogs = [
   'how-to-make-a-pcb',
@@ -26,17 +27,19 @@ const Bloglist = () => (
 
 
 export default function Blogpage() {
+  useEffect(() => {
+    alert("This page is under construction");
+  }, []);
+
   return (
     <>
-      <div className="py-8 flex flex-col min-h-screen  px-8 sm:px-48 sm:py-18" onCopy={CopyHandler}>
+      <div className="py-8 flex flex-col min-h-screen px-8 sm:px-48 sm:py-18" onCopy={CopyHandler}>
         <div>
           <h1 className="text-xl sm:text-3xl mb-5">BLOGS</h1>
         </div>
 
-        <Bloglist/>
-      </div>  
-      
+        <Bloglist />
+      </div>
     </>
-    
   );
 }
