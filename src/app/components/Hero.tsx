a910a0b3d9ceff41c39f0a04cd13d417bb9caabf
Arjav Jain <arjavjain0703@gmail.com>
2026-05-19 20:36:53 +0530

    update my details

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,9 @@ export default function Hero() {
     <section className="nes-container is-dark is-rounded ">
       <p className="text-xl py-5 nes-text text-yellow-200">
         Hey! I'm Arjav, a 16 year old from India with a passion for technology
-        and creativity. Welcome to my personal website 🚀✨
+        and creativity. Welcome to my personal website 🚀✨ <br />I am a rust
+        developer who is still learning new things. I am also an active
+        communnity volunteer at Hackclub.
       </p>
       <StuffILike />
     </section>
@@ -15,10 +17,8 @@ function StuffILike() {
     <div className=" ">
       {/*<h1 className="nes-text is-primary text-2xl">Stuff I Like</h1>*/}
       <p className="nes-text text-xl text-yellow-200">
-        In my free time, I enjoy reading fiction books, watching movies, and
-        playing video games. Some noteble mentions from the video games category
-        would be COD Black Ops, GTA V, Resident Evil 4, Rise of the Tomb Raider,
-        and Cyberpunk 2077
+        I enjoy storyplayer games, fiction books, and photography in my free
+        time.
       </p>
     </div>
   );
