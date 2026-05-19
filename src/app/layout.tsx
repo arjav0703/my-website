36e0aaff204c4121b3f05f322a731baa599bf22a
Arjav Jain <arjavjain0703@gmail.com>
2026-05-19 15:31:20 +0530

    update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import MyNav from "./components/nav";
 import MyFooter from "./components/footer";
 // script.js
-import "nes.css/css/nes.min.css";
 import RenderCharacter from "./components/Character";
 
 export const metadata: Metadata = {
